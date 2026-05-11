284280301f96c4934d8746ad1e1c064d197477c1
DINAR NATO <dnaliaka365@gmail.com>
2026-05-11 12:26:59 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        present = set()
+        for i in nums:
+            if i in present:
+                return True
+            present.add(i)
+        return False
+        
